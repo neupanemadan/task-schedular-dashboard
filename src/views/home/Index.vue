c4a6e96c3813197919ef9e17ffe7b47e71ebefaa
Madan Neupane <nmadan692@gmail.com>
2023-08-10 13:04:47 +0900

    updated schedular

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -1,20 +1,25 @@
 <template>
     <n-row gutter="12">
       <h1>Home</h1>
+      <p>{{ tasks }}</p>
     </n-row>
   </template>
   <script>
   import { defineComponent, ref} from "vue";
+import { mapActions, mapState } from "pinia"
   
   export default defineComponent({
     computed: {
+      ...mapState(taskStore, ["tasks"])
     },
-  
+      
   
     methods: {
+      ...mapActions(taskStore, ["fetchTasks"]),
   
     },
     mounted() {
+      this.fetchTasks()
     }
   });
   </script>
